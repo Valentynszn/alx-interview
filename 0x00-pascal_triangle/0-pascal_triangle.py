7885822b09f3684b40b7f75261a6e8c2036acf6e
valentynszn <valentineogbuagu86@gmail.com>
2024-07-26 03:32:02 +0000

    updated

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''A function to return Pascal's Triangle'''
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows of Pascal's Triangle to generate.
+
+    Returns:
+        lists of int: A list containing n lists of rows of Pascals Triangle
+    """
+    if n == 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
