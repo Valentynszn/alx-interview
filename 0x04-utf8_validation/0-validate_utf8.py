f76e35f083d17a3d45b000b294d597daf8bb840c
valentynszn <valentineogbuagu86@gmail.com>
2024-08-23 04:14:09 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,59 +1,39 @@
 #!/usr/bin/python3
-"""
-let's define if the given data is UTF-8
-"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
+    """Determines if a given data set
+    represents a valid utf-8 encoding
     """
-    check th list of integers are UTF-8
-    """
-    ignore = 0
-    dist = len(data)
-    for x in range(dist):
-        if ignore > 0:
-            ignore -= 1
-            continue
-        if type(data[x]) != int or data[x] < 0 or data[x] > 0x10ffff:
-            return False
-        elif data[x] <= 0x7f:
-            ignore = 0
-        elif data[x] & 0b11111000 == 0b11110000:
-            utf_span = 4
-            if dist - 1 >= utf_span:
-                next_body = list(map(
-                    lambda y: y & 0b11000000 == 0b10000000,
-                    data[x + x: x + utf_span],
-                ))
-                if not all(next_body):
-                    return False
-                ignore = utf_span - 1
-            else:
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-        elif data[x] & 0b11110000 == 0b11100000:
-            utf_span = 3
-            if dist - x >= utf_span:
-                next_body = list(map(
-                    lambda y: y & 0b11000000 == 0b10000000,
-                    data[x + 1: x + utf_span],
-                ))
-                if not all(next_body):
-                    return False
-                ignore = utf_span - 1
-            else:
-                return False
-        elif data[x] & 0b11100000 == 0b11000000:
-            utf_span = 2
-            if dist - x >= utf_span:
-                next_body = list(map(
-                    lambda y: y & 0b11000000 == 0b10000000,
-                    data[x + 1: x + utf_span],
-                ))
-                if not all(next_body):
-                    return False
-                ignore = utf_span - 1
-            else:
-                return False
+
         else:
-            return False
-    return True
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
